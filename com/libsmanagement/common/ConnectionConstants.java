c925369ebf64416acb425cc380f66e835ff5cfdd
Sohel Shaikh <rebht78@gmail.com>
2017-11-29 11:04:09 +0530

    Create ConnectionConstants.java

---
+++ com/libsmanagement/common/ConnectionConstants.java
@@ -0,0 +1,11 @@
+package com.libsmanagement.common;
+
+/**
+ *
+ * @author sohel1
+ */
+public class ConnectionConstants {
+    public static final String URL = "";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+}
